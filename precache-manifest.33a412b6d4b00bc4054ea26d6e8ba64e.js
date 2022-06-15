6aef0bf196ec5ed44b61053a2c01d48bded931fa
Numan-star <5125.2019.gct@gmail.com>
2022-06-16 00:06:05 +0500

    Updates

---
+++ precache-manifest.33a412b6d4b00bc4054ea26d6e8ba64e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e917c97927e7f595893b",
+    "url": "/Todo-List/static/css/main.2042f88d.chunk.css"
+  },
+  {
+    "revision": "e917c97927e7f595893b",
+    "url": "/Todo-List/static/js/main.e917c979.chunk.js"
+  },
+  {
+    "revision": "521dee7d6a1a5478516b",
+    "url": "/Todo-List/static/js/1.521dee7d.chunk.js"
+  },
+  {
+    "revision": "1b9852ba5058245a8866",
+    "url": "/Todo-List/static/js/2.1b9852ba.chunk.js"
+  },
+  {
+    "revision": "1749597c1e43f70aee25",
+    "url": "/Todo-List/static/js/runtime~main.1749597c.js"
+  },
+  {
+    "revision": "96d22f9b3e4404062c5eda248349811c",
+    "url": "/Todo-List/index.html"
+  }
+];
